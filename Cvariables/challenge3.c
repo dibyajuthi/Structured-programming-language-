6bc4c0137fe2653b3119b1aa844a2b23b499c8c6
dibyajuthi <juthidibya26@gmail.com>
2026-09-06 00:46:25 +0600

    add 3rd exercise

---
+++ Cvariables/challenge3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int length=10;
+    int width=5;
+    int area;
+
+    area= length*width; 
+
+    printf("Length:%d\n", length);
+    printf("Width:%d\n", width);
+    printf("Area of a rectangle:%d", area);
+    return 0;
+}
