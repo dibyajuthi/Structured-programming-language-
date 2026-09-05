508376a919a71a931b47524aa9e2bf1c8d951df4
dibyajuthi <juthidibya26@gmail.com>
2026-09-06 00:25:18 +0600

    add 9th example

---
+++ Cvariables/reallife.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int stdID = 15;
+    int stdAge=19;
+    float stdFee=75.25;
+    char stdGrade='B';
+
+    printf("Student ID:%d\n", stdID);
+    printf("Student age:%d\n", stdAge);
+    printf("Student fee:%f\n", stdfee);
+    printf("Student grade:%c", stdGrade);
+
+    return 0;
+}
