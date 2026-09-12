0131a9d678f34c55af3908f2472ae7677ca5274a
dibyajuthi <juthidibya26@gmail.com>
2026-09-12 13:38:22 +0600

    add 4th exercise

---
+++ Cdatatypes/challenge4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    
+    int studentID= 45;
+    float score = 79.999;
+    char grade = 'A';
+
+    printf("%d\n", studentID);
+    printf("%f\n", score);
+    printf("%c\n", grade);
+
+    return 0;
+}
