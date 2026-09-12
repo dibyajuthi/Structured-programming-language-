692ee25c6bd56df1b8612b60ca2abdc040c45fc6
dibyajuthi <juthidibya26@gmail.com>
2026-09-12 10:49:27 +0600

    add 1st example

---
+++ Cdatatypes/formatspecifier.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    //create variables
+    int myNum=5;    //Integer
+    float myFloatNum=5.99;  //Floating Point
+    char myLetter='D';  //Character
+
+    //Print variables
+    printf("%d\n", myNum);
+    printf("%f\n", myFloatNum);
+    printf("%c\n", myLetter);
+
+    return 0;
+
+}
