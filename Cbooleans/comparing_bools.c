eedaa21d7315e731631e7265c4f4db1eced5ff4e
dibyajuthi <juthidibya26@gmail.com>
2026-09-14 01:06:47 +0600

    add 4th example

---
+++ Cbooleans/comparing_bools.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main()
+{
+    bool isHamburgerTasty= true;
+    bool isPizzaTasty= true;
+
+    // Find out if both hamburger and pizza are tasty
+    printf("%d", isHamburgerTasty == isPizzaTasty);
+
+    return 0;
+}
