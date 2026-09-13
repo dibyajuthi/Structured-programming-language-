b29e72523db5ff2f41fb9889b5de8c668c4c0f8d
dibyajuthi <juthidibya26@gmail.com>
2026-09-14 00:39:45 +0600

    add 5th exercise

---
+++ Coperators/challenge5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  
+    // Write itemPrice
+	int itemPrice=10;
+  // Write shippingCost
+	int shippingCost=20;
+  // Write sum(itemPrice+shippingCost)
+	int sum= itemPrice + shippingCost;
+  
+    // Print sum
+	printf("%d\n", sum);
+    
+  return 0;
+}
