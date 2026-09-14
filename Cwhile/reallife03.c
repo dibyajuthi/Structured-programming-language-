42938618c4486e059d9be3b3ca08214dec6ac972
dibyajuthi <juthidibya26@gmail.com>
2026-09-14 19:06:57 +0600

    add 7th example

---
+++ Cwhile/reallife03.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+
+    int numbers = 12345;
+    int revNumbers = 0;
+
+
+    while (numbers){
+    revNumbers = revNumbers * 10 + numbers % 10;
+    numbers /= 10;
+    } 
+
+    return 0;
+}
