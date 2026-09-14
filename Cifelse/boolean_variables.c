ad6718319995cae86670ba851fff260692c907c8
dibyajuthi <juthidibya26@gmail.com>
2026-09-14 18:08:52 +0600

    add 3rd example

---
+++ Cifelse/boolean_variables.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main()
+{
+    int x=20,y=18;
+
+    bool isGreater=x>y;
+
+    if(isGreater){
+        printf("x is greater than y");
+    }
+    
+    return 0;
+}
