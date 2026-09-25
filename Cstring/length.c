8e1f83a187c548b67e060d2c51a1a48793902352
dibyajuthi <juthidibya26@gmail.com>
2026-09-25 10:34:40 +0600

    add 9th example

---
+++ Cstring/length.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    char alphabet[]="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    printf("%zu\n", strlen(alphabet));
+    printf("%zu\n", sizeof(alphabet));
+
+    return 0;
+}
